eeb47707e41cc774a99764eec9899b63190c798f
Amedes Jr <amedes@gmail.com>
2023-09-18 15:34:59 -0300

    bookstore -> Set, 18 de 2023 -> first commit

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+
+Sell.all.each { |s| s.destroy }
+Book.all.each { |b| b.destroy }
+
+book = Book.create(name: 'Harry Potter', price: 24.99)
+
+5.times do
+  book.sells << Sell.create(quantity: rand(2..15), day: Sell::DAYS[rand(0..(Sell::DAYS.length-1))])
+end
