e0cd2e54514d1486a8526becd930251bb5da98f7
Amedes Jr <amedes@gmail.com>
2023-10-18 15:50:45 -0300

    bookstore -> Out, 18 de 2023 -> Aula 78

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -6,6 +6,8 @@ class Sell < ApplicationRecord
 
   after_create :set_total
 
+  after_create_commit :update_charts
+
   DAYS = %w[MONDAY TUESDAY WEDNESDAY THURSDAY FRIDAY SATURDAY SUNDAY].freeze
 
   private
@@ -14,5 +16,26 @@ class Sell < ApplicationRecord
     revenue = quantity * book.price
     update(total: revenue)
   end
+
+  def update_charts
+    broadcast_replace_to('sells_charts_channel',
+      partial: "/dashboard/charts/days/chart",
+      locals: {},
+      target:'days_chart'
+    )
+
+    broadcast_replace_to('sells_charts_channel',
+      partial: "/dashboard/charts/revenue/chart",
+      locals: {},
+      target:'revenue_chart'
+    )
+
+    broadcast_replace_to('sells_charts_channel',
+      partial: "/dashboard/charts/books/chart",
+      locals: {book: book },
+      target:"book_#{book.id}"
+    )
+  end
+
 
 end
